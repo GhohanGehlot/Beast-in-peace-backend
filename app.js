96069480278236437f2bfdc02304cbc45fd76c30
Your Name <you@example.com>
2025-10-02 10:02:30 +0530

    Added and config dotenv

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
@@ -6,6 +9,8 @@ app.get('/' , (req , res) => {
     res.send("Hey the server is working !!");
 })
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000 , (req , res) => {
+    console.log(`your server is connected at ${process.env.PORT}`);
+});
 
 
